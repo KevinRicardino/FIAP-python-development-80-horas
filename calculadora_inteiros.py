f8d0560b443ccf08ae23d79f71d3bd12b13321d9
KevinRicardino <kevin.j.bernardes@gmail.com>
2025-10-30 07:44:42 -0300

    Atualização das Variáveis e tipos de dados

--- calculadora_inteiros.py
+++ calculadora_inteiros.py
@@ -1,8 +1,20 @@
-print('==== Boas vindas ao calculador de inteiros ====')
-primeiro_valor = input('Por favor, informe o primeiro valor: ')
-segundo_valor = input('Por favor, informe o segundo valor: ')
-print(type(primeiro_valor))
-print(type(segundo_valor))
-soma = primeiro_valor + segundo_valor
+print("==== Boas vindas ao calculador de inteiros ====\n")
+primeiro_valor = int(input("Por favor, informe o primeiro valor: "))
+print(f"Tipo do primeiro valor: {type(primeiro_valor).__name__}")
+segundo_valor = int(input("Por favor, informe o segundo valor: "))
+print(f"Tipo do segundo valor: {type(segundo_valor).__name__}")
 
-print('O resultado da soma foi ' + soma)
+soma = primeiro_valor + segundo_valor
+subtracao = primeiro_valor - segundo_valor
+divisao = primeiro_valor / segundo_valor
+multiplicacao = primeiro_valor * segundo_valor
+
+print("\n==== Resultados das operações ====")
+# print("\nO resultado da soma foi: {}".format(soma))
+# print(f"\nO resultado da soma foi: {soma}")
+print(
+    f"\nO resultado da soma foi: {soma}"
+    f"\nO resultado da subtração foi: {subtracao}"
+    f"\nO resultado da divisão foi: {divisao:.2f}"
+    f"\nO resultado da multiplicação foi: {multiplicacao}"
+)
