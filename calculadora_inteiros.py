c784a423f4ac257179360eb66c6bc5064a942d41
KevinRicardino <kevin.j.bernardes@gmail.com>
2025-10-30 04:15:01 -0300

    Primeiros passos

---
+++ calculadora_inteiros.py
@@ -0,0 +1,8 @@
+print('==== Boas vindas ao calculador de inteiros ====')
+primeiro_valor = input('Por favor, informe o primeiro valor: ')
+segundo_valor = input('Por favor, informe o segundo valor: ')
+print(type(primeiro_valor))
+print(type(segundo_valor))
+soma = primeiro_valor + segundo_valor
+
+print('O resultado da soma foi ' + soma)
