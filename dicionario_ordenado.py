6c66b2a8925b0ee8bf57873e436c828aec5f99b7
KevinRicardino <kevin.j.bernardes@gmail.com>
2025-11-05 07:34:37 -0300

    Estudo de ordem em dicionários

---
+++ dicionario_ordenado.py
@@ -0,0 +1,39 @@
+# Importação do OrderedDict
+from collections import OrderedDict
+
+# Criação
+dicionario_ordenado = OrderedDict()
+print(dicionario_ordenado)
+
+# Adicionando chaves e valores
+dicionario_ordenado["NOME"] = "Iphone"
+dicionario_ordenado["MARCA"] = "Apple"
+dicionario_ordenado["MODELO"] = "14 PRO MAX"
+
+# Percorrendo para verificar a ordem
+for chave, valor in dicionario_ordenado.items():
+    print(f"{chave} -- {valor}")
+
+# Alterando um item
+dicionario_ordenado["MARCA"] = "Maça"
+print()
+
+# Percorrendo para verificar a ordem
+for chave, valor in dicionario_ordenado.items():
+    print(f"{chave} -- {valor}")
+
+# Removendo um item
+dicionario_ordenado.pop("MARCA")
+print()
+
+# Percorrendo para verificar a ordem
+for chave, valor in dicionario_ordenado.items():
+    print(f"{chave} -- {valor}")
+
+# Reinserindo um item neste dicionário
+dicionario_ordenado["MARCA"] = "Apple"
+print()
+
+# Percorrendo para verificar a ordem
+for chave, valor in dicionario_ordenado.items():
+    print(f"{chave} -- {valor}")
